65980c6766f8e1fd5946c946c68651662c2f512f
wuliang <xupt_wl@163.com>
2019-04-16 22:10:29 +0800

    折叠类中所有的方法,这样看起来更加方便

--- app/src/main/java/com/dfxh/wang/coderead/MainActivity.java
+++ app/src/main/java/com/dfxh/wang/coderead/MainActivity.java
@@ -6,11 +6,21 @@ import android.os.Bundle;
 import com.dfxh.wang.coderead.utils.L;
 
 public class MainActivity extends AppCompatActivity {
-
+//mac 折叠所有方法是 command alt -
+    //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         L.d("daodishinali");
+        int i=100;
+        for (int i1 = 0; i1 < i; i1++) {
+
+        }
+        boolean b=false;
+        if (b) {
+
+        }
+
     }
 }
